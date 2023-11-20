e781881935715d877533b37fe7ceaeba12815f29
Graciela de la Cruz <graciela_cruz@live.com.mx>
2023-11-20 01:04:23 -0600

    header-menu

--- src/templates/layout.jsx
+++ src/templates/layout.jsx
@@ -1,30 +1,29 @@
 import Grid from '@mui/material/Unstable_Grid2';
-import {Box, CssBaseline} from "@mui/material";
-
+import {Box, CssBaseline } from '@mui/material';
 function Layout({children}) {
+    const [header, menu] = children
     return (
-        <Box style={{display: "flex", height: "100vh"}}>
-           <CssBaseline />
+        <Box style= {{display: "flex", height: "100vh" }}>
+            <CssBaseline />
             <Grid container sx={{flexGrow: 1}}>
-              <Grid item flexBasis={220}>
-                 Menu goes here 
-              </Grid>
-              <Grid item flex={1}>
-                <Grid xs={12}>HEADER</Grid>
-                <Grid container spacing={3}> 
-                <Grid xs={3}>PKI</Grid>
-                <Grid xs={3}>PKI</Grid>
-                <Grid xs={3}>PKI</Grid>
-                <Grid xs={3}>PKI</Grid>
-              </Grid>
-              <Grid container spacing={3}>
-                <Grid container xs={8}>Container 1</Grid>
-                <Grid container xs={4}>Container 2</Grid>
+                <Grid item flexBasis={220}>{menu}</Grid>
+                <Grid item flex={1}>
+                    <Grid xs={12}>
+                        {header}
+                    </Grid>
+                    <Grid container spacing={3}>
+                        <Grid xs={3}>PKI</Grid>
+                        <Grid xs={3}>PKI</Grid>
+                        <Grid xs={3}>PKI</Grid>
+                        <Grid xs={3}>PKI</Grid>
+                    </Grid>
+                    <Grid container spacing={3}>
+                        <Grid xs={8}>Container 1</Grid>
+                        <Grid xs={4}>Container 2</Grid>
+                    </Grid>
                 </Grid>
-              </Grid>
             </Grid>
-        </Box> 
+        </Box>
     );
 }
-
 export default Layout;
